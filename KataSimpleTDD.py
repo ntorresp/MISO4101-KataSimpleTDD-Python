f65f8a31265bd4b82d24e5747687a94bc7347cf8
Nelson <mrrudeboy236>
2020-03-15 11:55:11 -0500

    Paso 3 - Se modifica el test del numero de elementos para cuando la cadena tiene dos numeros (Verde)

--- KataSimpleTDD.py
+++ KataSimpleTDD.py
@@ -4,5 +4,8 @@ class KataSimpleTDD:
     def contar(self, cadena):
         if cadena == "":
             return 0
+        elif "," in cadena:
+            cadenaSplit= cadena.split(",")
+            return len(cadenaSplit)
         else:
             return len(cadena)
