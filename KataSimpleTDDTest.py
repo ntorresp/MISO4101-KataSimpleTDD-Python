db482f0a90d6e43623aefa8d5a8ec5e44ae53d3a
Nelson <mrrudeboy236>
2020-03-15 12:36:02 -0500

    Paso 11 - Se crea el test del numero de elementos, el minimo y el maximo para cuando la cadena tiene dos numeros (rojo)

--- KataSimpleTDDTest.py
+++ KataSimpleTDDTest.py
@@ -38,3 +38,6 @@ class KataSimpleTDDTest(TestCase):
     def test_maximo_string_con_una_cadena(self):
         self.assertEqual(KataSimpleTDD().maximo("1"),1,"Maximo de una cadena")
         self.assertEqual(KataSimpleTDD().maximo("2"),2,"Maximo de un numero")
+
+    def test_maximo_string_con_dos_numeros(self):
+        self.assertEqual(KataSimpleTDD().maximo("5,8"),8,"Maximo de dos numeros")
