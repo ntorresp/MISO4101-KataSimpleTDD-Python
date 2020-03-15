f909cfe40ddf34ea55c4b40d977507d43ca7c07d
Nelson <mrrudeboy236>
2020-03-15 11:37:38 -0500

    Paso 3 - Se crea el test del numero de elementos para cuando la cadena tiene un numero (Rojo)

--- KataSimpleTDDTest.py
+++ KataSimpleTDDTest.py
@@ -8,3 +8,7 @@ class KataSimpleTDDTest(TestCase):
         
     def test_contar_unacadena(self):
         self.assertEqual(KataSimpleTDD().contar("1"), 1,"Un numero")
+
+    def test_contar_cadenaConUnNumero(self):
+        self.assertEqual(KataSimpleTDD().contar("1"), 1,"Un numero")
+        self.assertEqual(KataSimpleTDD().contar("2"), 1,"Un numero")
